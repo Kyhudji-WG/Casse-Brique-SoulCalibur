5608d2a6d03ecd5758a4a7fd221c750f3153757b
unknown <wgabali@gamingcampus.local>
2023-11-06 12:47:15 +0100

    inisialisation de la sfml

--- Casse-Brique-SoulCalibur/Casse-Brique-SoulCalibur.cpp
+++ Casse-Brique-SoulCalibur/Casse-Brique-SoulCalibur.cpp
@@ -2,11 +2,54 @@
 //
 
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
-int main()
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+    //Création d'une fenêtre
+    sf::RenderWindow oWindow(sf::VideoMode(640, 480), "SFML");
+
+    //Création d'un cercle de radius 100
+    sf::CircleShape oCircle(100.f);
+    //A la position 0, 0
+    oCircle.setPosition(0.f, 0.f);
+    //Et de couleur verte
+    oCircle.setFillColor(sf::Color::Green);
+
+
+    //Création d'un rectangle de taille 50, 50
+    sf::RectangleShape oRectangle(sf::Vector2f(50.f, 50.f));
+    //A la position 100, 100
+    oCircle.setPosition(100.f, 100.f);
+    //Et de couleur rouge
+    oRectangle.setFillColor(sf::Color::Red);
+
+    //GameLoop
+    while (oWindow.isOpen())
+    {
+        //EVENT
+        sf::Event oEvent;
+        while (oWindow.pollEvent(oEvent))
+        {
+            if (oEvent.type == sf::Event::Closed)
+                oWindow.close();
+        }
+
+        //UPDATE
+
+        //DRAW
+        oWindow.clear();
+
+        oWindow.draw(oCircle);
+        oWindow.draw(oRectangle);
+
+        oWindow.display();
+    }
+
+    return 0;
 }
+
+
 
 // Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
 // Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
